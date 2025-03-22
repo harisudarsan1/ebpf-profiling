3c212ced951efbb644d3e0b1030dfcfe97d8432f
Harisudarsan <sudarshanhari561@gmail.com>
2025-03-23 00:33:11 +0530

    evrything is working

--- src/sysprof.c
+++ src/sysprof.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 #include <sys/resource.h>
 #include <bpf/libbpf.h>
-#include "ebpf/sysprof-bpf.skel.h"
+#include <sysprof.skel.h> // From ebpf/ directory
 
 static int libbpf_print_fn(enum libbpf_print_level level, const char *format, va_list args)
 {
@@ -18,7 +18,7 @@ int main(int argc, char **argv)
 	libbpf_set_print(libbpf_print_fn);
 
 	/* Open BPF application */
-	skel = sysprof_bpf_bpf__open();
+	skel = sysprof_bpf__open();
 	if (!skel) {
 		fprintf(stderr, "Failed to open BPF skeleton\n");
 		return 1;
@@ -28,14 +28,14 @@ int main(int argc, char **argv)
 	/* skel->bss->my_pid = getpid(); */
 
 	/* Load & verify BPF programs */
-	err = sysprof_bpf_bpf__load(skel);
+	err = sysprof_bpf__load(skel);
 	if (err) {
 		fprintf(stderr, "Failed to load and verify BPF skeleton\n");
 		goto cleanup;
 	}
 
 	/* Attach tracepoint handler */
-	err = sysprof_bpf_bpf__attach(skel);
+	err = sysprof_bpf__attach(skel);
 	if (err) {
 		fprintf(stderr, "Failed to attach BPF skeleton\n");
 		goto cleanup;
@@ -51,6 +51,6 @@ int main(int argc, char **argv)
 	}
 
 cleanup:
-	sysprof_bpf_bpf__destroy(skel);
+	sysprof_bpf__destroy(skel);
 	return -err;
 }
